e21de1441d1de75eb7f1c5896077ee5b8b5bcfb6
Alexfar <Alexfar1111@gmail.com>
2018-07-22 15:45:23 +0300

    Temp commit

--- src/excel-parser.js
+++ src/excel-parser.js
@@ -1,23 +1,163 @@
 const path = require('path');
 const Excel = require('exceljs');
-const httpRequestService = require('./http-request-service');
+const http = require('./http-request-service');
 
 const excelFilePath = path.resolve('./public/Experty.xlsx');
+const artistFilePath = path.resolve('./public/Artists.xlsx');
+
+const customSeparator = ',';
 
 
 let workbook = new Excel.Workbook();
+let artistsWorkbook = new Excel.Workbook();
+
+let artistsWorksheet;
+let albumsWorksheet;
+let tracksWorksheet;
+
+
 workbook.xlsx.readFile(excelFilePath)
     .then(() => {
-      const groupArr =  workbook.worksheets[0].columns[0].values.filter((groupName) => groupName);
-      groupArr.forEach((groupName) => {
-        httpRequestService.getSuggestDataByGroupName(groupName).then((resp) => {
-          const entities = resp.entities;
-          if(entities.length) {
-            const artistData = entities.find((val) => val.hasOwndProperty('type') && val.results.length && val.results[0]);
-                        // get artist
-              // save to groups.csv and get albums
-          }
-        })
-      })
+        artistsWorksheet = artistsWorkbook.addWorksheet('Artists');
+        albumsWorksheet = artistsWorkbook.addWorksheet('Albums');
+        tracksWorksheet = artistsWorkbook.addWorksheet('Tracks');
+        setArtistWorksheetColumns();
+        setAlbumsWorksheetColumns();
+        setTracksWorksheetColumns();
+
+        const groupArr = workbook.worksheets[0].columns[0].values.filter((groupName) => groupName);
+        return Promise.all(groupArr.map((groupName) => {
+            return new Promise((resolve) => {
+                http.getSuggestDataByGroupName(groupName).then(
+                    (resp) => {
+                        const entities = resp.data.entities;
+                        if (entities.length) {
+                            const artistId = entities.find((val) => {
+                                return val.hasOwnProperty('type') && val.type === 'artist' && val.results && val.results.length
+                            }).results[0].artist.id;
+
+                            http.getArtistDataById(artistId).then((resp) => {
+                                addRowToArtistWorksheet(resp);
+                                http.getAlbumsIdsByArtistId(artistId).then((albumIds) => {
+                                    Promise.all(
+                                        albumIds.map((albumId) => {
+                                            http.getAlbumDataById(albumId).then((resp) => {
+                                                addRowToAlbumsWorksheet(artistId, albumId, resp);
+                                            })
+                                        })
+                                    ).then(() => {
+                                        resolve();
+                                    })
+                                })
+                            });
+                        }
+                        else {
+                            resolve();
+                        }
+                })
+            });
+        }))
+    })
+    .then(() => {
+        artistsWorkbook.xlsx.writeFile(artistFilePath);
+    })
+
+
+const getSimilarSocialCounters = (socialCountersObj) => {
+    let socialCounterString = '';
+    const socialKeys = Object.keys(socialCountersObj);
+    if (socialKeys.length) {
+        socialCounterString = `${socialKeys[0]}:${socialCountersObj[socialKeys[0]]}`;
+        for (let i = 1, il = socialKeys.length; i < il; ++i) {
+            socialCounterString += `,${socialKeys[i]}:${socialCountersObj[socialKeys[i]]}`
+        }
+    }
+    return socialCounterString;
+}
+
+
+const getSimilarFromArtistData = (artistData) => {
+    return artistData.similar.map((similarObj) => similarObj.name).join(customSeparator);
+}
+
+const setArtistWorksheetColumns = () => {
+    artistsWorksheet.columns = [
+        { header: 'Id', key: 'id' },
+        { header: 'Name', key: 'name' },
+        { header: 'SimilarArtists.', key: 'similarArtists' },
+        { header: 'Info.', key: 'artistInfo' },
+        { header: 'Tags.', key: 'artistTags' },
+        { header: 'Country.', key: 'artistCountry' },
+        { header: 'SocialCounters.', key: 'artistSocialCounters' }
+    ];
+}
+
+
+const setAlbumsWorksheetColumns = () => {
+    albumsWorksheet.columns = [
+        { header: 'ArtistId', key: 'artistId' },
+        { header: 'Id', key: 'id' },
+        { header: 'Name', key: 'name' },
+        { header: 'ReleaseDate', key: 'releaseDate' },
+        { header: 'Label.', key: 'label' }
+    ];
+}
+
+const setTracksWorksheetColumns = () => {
+    tracksWorksheet.columns = [
+        { header: 'Id', key: 'id' },
+        { header: 'Name', key: 'name' },
+        { header: 'AlbumId', key: 'albumId' },
+        { header: 'ArtistId', key: 'artistId' }
+    ];
+}
+
+const addRowToArtistWorksheet = (resp) => {
+    const respData = resp.data;
+    const artistData = respData.artist;
+
+    const id = artistData.id
+    const name = artistData.name;
+    const similarArtists = getSimilarFromArtistData(respData);
+    const artistInfo = artistData.description.text;
+    const artistTags = artistData.genres.join(customSeparator);
+    const artistCountry = artistData.countries.join(customSeparator);
+    const artistSocialCounters = getSimilarSocialCounters(respData.socialCounters);
+
+    artistsWorksheet.addRow({
+        id,
+        name,
+        similarArtists,
+        artistInfo,
+        artistTags,
+        artistCountry,
+        artistSocialCounters
     });
+}
+
+const addRowToAlbumsWorksheet = (artistId, albumId, resp) => {
+    const respData = resp.data;
+
+    albumsWorksheet.addRow({
+        artistId,
+        id: albumId,
+        name: respData.title,
+        releaseDate : respData.releaseDate,
+        labels: respData.labels.map((labelObj) => labelObj.name).join(customSeparator),
+    });
+
+    addTracksToWorksheet(respData.volumes, artistId, albumId);
+
+}
+
+const addTracksToWorksheet = (tracksArr, artistId, albumId) => {
+    tracksArr.map((trackObj) => {
+        tracksWorksheet.addRow({
+            id: trackObj.id,
+            name: trackObj.title,
+            albumId : albumId,
+            artistId: artistId
+        });
+    })
+}
 
--- src/http-request-service.js
+++ src/http-request-service.js
@@ -1,26 +1,61 @@
 const axios = require('axios');
 
 const yandexHelper = require('./yandex-helper');
+const userConfig = require('./user-config');
+
+const sign = userConfig.sign;
+const cookie = userConfig.cookie;
 
 
-const yandexArtistUrl = yandexHelper.artistUrl;
 const yandexSuggestUrl = yandexHelper.suggestUrl;
 
+const getSuggestDataByGroupName = (groupName) => {
+  const suggestRequestBody = yandexHelper.buildSuggestRequestBody(groupName, sign);
 
-static const getSuggestDataByGroupName = (groupName) => {
-  const suggestRequestBody = yandexHelper.buildSuggestRequestBody(groupName);
-
-  axios({
+  return axios({
     method: 'post',
     url: yandexSuggestUrl,
     data: suggestRequestBody,
     headers: {
-      Cookie: 'yandexuid=1964478881501233192; _ym_uid=1502261297154585437; mda=0; fuid01=5993031d4d38d607.shsyKsTA0C2YetpBe3ySX8Vz-U-TJNX8jBEQW4hW30hanYa5DqfCqhi0ND1WzK89F0qyWJKXY3q3Y0Klw243BZYqYjY0zw68ax4ByvXJAghgGAP2rhAqDrlXUT5wkiOg; my=YwA=; L=B0p5BnZtQlFSB2kIU19kYlxeB3dPRHxZJQg3SyEgMQ==.1526471196.13501.384876.a05a962a6f534098db7d0f3ffdb4bbda; yandex_login=A1exfar; i=rDKoUWun2zvYrGUbOsSREnTS85ytcoNtyjyDMWkcrKc+VJSOYnrWt5RSEz/botBcfOYGHnCSB8qdVyarP1divCNQVGI=; _ym_d=1531131102; Session_id=3:1531836128.5.0.1526471196393:CzgV2Q:1c.1|440553800.0.2|184684.577873.HfjuZlAl1iXQnJF_W8lB-OLaEwI; sessionid2=3:1531836128.5.0.1526471196393:CzgV2Q:1c.1|440553800.0.2|184684.304974.tnPAqz5adSdW_52p6Zi-MWFLCvY; _ym_isad=1; yp=1816593192.yrts.1501233192#1841831196.udn.cDpBbGV4ZXkgRmFyYm90a28%3D#1547246050.szm.1:1920x1080:1920x974#1563025477.as.1; device_id="a2abab550fea6f33fd023d4676e90293e7df5848c"; lastVisitedPage=%7B%22440553800%22%3A%22%2Fusers%2FA1exfar%2Fplaylists%22%7D; _ym_visorc_1028356=b'
+      Cookie: cookie
     }
+  });
+}
+
+const getArtistDataById = (artistId) => {
+  const artistUrl = yandexHelper.buildArtistInfoUrl(artistId);
+
+  return axios({
+    method: 'get',
+    url: artistUrl,
+  });
+}
+
+const getAlbumsIdsByArtistId = (artistId) => {
+  const artistUrl = yandexHelper.buildArtistAlbumsUrl(artistId);
+
+  return axios({
+    method: 'get',
+    url: artistUrl,
+  }).then((resp) => {
+    return resp.data.albumIds;
+  });
+}
+
+
+const getAlbumDataById = (albumId) => {
+  const albumUrl = yandexHelper.buildAlbumUrl(albumId);
+
+  return axios({
+    method: 'get',
+    url: albumUrl,
   });
 }
 
 
 module.exports = {
-  getSuggestDataByGroupName
+  getSuggestDataByGroupName,
+  getArtistDataById,
+  getAlbumsIdsByArtistId,
+  getAlbumDataById
 }
